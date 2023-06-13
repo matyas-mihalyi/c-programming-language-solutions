8514c215de92fa289c106d0b8b64a6dcfd6ee399
Matyas Mihalyi <mihalyi.istvan.m@outlook.hu>
2023-06-13 21:45:15 +0200

    add solution for 1-3

---
+++ 1-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Modify the temperature conversion program
+to print a heading above the table */
+main()
+{
+  float fahr, celsius;
+  float lower, upper, step;
+
+  lower = 0; /* lower limit of temperatuire scale */
+  upper = 300; /* upper limit */
+  step = 20; /* step size */
+
+  fahr = lower;
+  printf("%3s %6s\n", "F", "C");
+  while (fahr <= upper) {
+  celsius= (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
